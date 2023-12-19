0c09455a6ed73698f74c8ca32836328f69c37086
dongze.wei <lainie.wei@qq.com>
2023-12-19 21:01:54 +0800

    init

--- src/main/java/com/docta/ai/hermes/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/docta/ai/hermes/security/oauth2/CustomOAuth2UserService.java
@@ -39,10 +39,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         }
     }
 
+    //OAuth2登录后操作
     private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
         OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
         if (StringUtils.isEmpty(oAuth2UserInfo.getEmail())) {
             throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
+        }
+        if (StringUtils.isEmpty(oAuth2UserInfo.getName())) {
+            throw new OAuth2AuthenticationProcessingException("Name not found from OAuth2 provider");
         }
 
         Optional<User> userOptional = userRepository.findByGoogleEmail(oAuth2UserInfo.getEmail());
